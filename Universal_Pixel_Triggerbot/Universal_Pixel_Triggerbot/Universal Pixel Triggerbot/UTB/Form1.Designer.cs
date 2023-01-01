167e0fcf4c13337fa739a981e355b06f410cc305
fortnitebacon2 <81583630+fortnitebacon2@users.noreply.github.com>
2023-01-01 23:39:19 +0100

    Trigger bot for any game made by me
    Trigger bot for any game made by me

---
+++ Universal_Pixel_Triggerbot/Universal_Pixel_Triggerbot/Universal Pixel Triggerbot/UTB/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace UTB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 77);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(129, 17);
+            this.checkBox2.TabIndex = 0;
+            this.checkBox2.Text = "Shoot for 500ms: OFF";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged_1);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(5, 137);
+            this.trackBar1.Maximum = 3;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Lime;
+            this.linkLabel1.Location = new System.Drawing.Point(6, 198);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(81, 13);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Join my Discord";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Triggerbot FOV";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "+";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(5, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 33);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(274, 225);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.checkBox2);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Universal_Pixel_Triggerbot/Universal_Pixel_Triggerbot/Universal Pixel Triggerbot/UTB/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections.Generic;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace UTB
+{
+    public partial class Form1 : MaterialForm
+    {
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vKey);
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int a, int b, int c, int d, int swed);
+
+        int leftDown = 0x02;
+        int leftUp = 0x04;
+
+        Color oldCol;
+        Color newCol;
+
+        Point xy = new Point(Screen.PrimaryScreen.Bounds.Width / 2 + 2, Screen.PrimaryScreen.Bounds.Height / 2 + 2);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Grey900, Primary.LightGreen300, Primary.Green800, Accent.Green700, TextShade.BLACK);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread tb = new Thread(trigger) { IsBackground = true };
+            tb.Start();
+        }
+
+        void trigger()
+        {
+
+
+            while (true)
+            {
+                if (GetAsyncKeyState(Keys.RButton) < 0)
+                {
+                    Thread.Sleep(10);
+                    oldCol = GetPixel(xy);
+                    Thread.Sleep(5);
+                    newCol = GetPixel(xy);
+                    if (oldCol.Equals(newCol) == false & checkBox2.Checked == false)
+                    {
+                        Thread.Sleep(1);
+                        mouse_event(leftDown, 0, 0, 0, 0);
+                        Thread.Sleep(10);
+                        mouse_event(leftUp, 0, 0, 0, 0);
+                        Thread.Sleep(2000);
+                    }
+
+                    if (oldCol.Equals(newCol) == false & checkBox2.Checked == true)
+                    {
+                        Thread.Sleep(1);
+                        mouse_event(leftDown, 0, 0, 0, 0);
+                        Thread.Sleep(500);
+                        mouse_event(leftUp, 0, 0, 0, 0);
+                        Thread.Sleep(2000);
+                    }
+                }
+                Thread.Sleep(2);
+            }
+        }
+
+        Color GetPixel(Point position)
+        {
+            using (var bitmap = new Bitmap(1, 1))
+            {
+                using (var graphics = Graphics.FromImage(bitmap))
+                {
+                    graphics.CopyFromScreen(position, new Point(0, 0), new Size(1, 1));
+                }
+                return bitmap.GetPixel(0, 0);
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar1.Value == 0)
+            {
+                xy = new Point(Screen.PrimaryScreen.Bounds.Width / 1 + 1, Screen.PrimaryScreen.Bounds.Height / 1 + 1);
+            }
+
+            if (trackBar1.Value == 1)
+            {
+                xy = new Point(Screen.PrimaryScreen.Bounds.Width / 2 + 2, Screen.PrimaryScreen.Bounds.Height / 2 + 2);
+            }
+
+            if (trackBar1.Value == 2)
+            {
+                xy = new Point(Screen.PrimaryScreen.Bounds.Width / 3 + 3, Screen.PrimaryScreen.Bounds.Height / 3 + 3);
+            }
+
+            if (trackBar1.Value == 3)
+            {
+                xy = new Point(Screen.PrimaryScreen.Bounds.Width / 4 + 4, Screen.PrimaryScreen.Bounds.Height / 4 + 4);
+            }
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged_1(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked) {
+                checkBox2.Text = "Shoot for 500ms: ON";
+            } else
+            {
+                checkBox2.Text = "Shoot for 500ms: OFF";
+            }
+        }
+
+        private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://discord.gg/jwt2AK8xKr");
+        }
+    }
+}
